1de9ea5060066dfb88e1c798f4f605a064e2d44e
Kevin Lui <mrkevinlui@gmail.com>
2022-11-16 16:58:26 -0800

    try uuid in place of uuidv4

--- spec/routes/addItem.spec.js
+++ spec/routes/addItem.spec.js
@@ -1,9 +1,9 @@
 const db = require('../../src/persistence');
 const addItem = require('../../src/routes/addItem');
 const ITEM = { id: 12345 };
-const uuid = require('uuid/v4');
+const uuid = require('uuid');
 
-jest.mock('uuid/v4', () => jest.fn());
+jest.mock('uuid', () => jest.fn());
 
 jest.mock('../../src/persistence', () => ({
     removeItem: jest.fn(),
